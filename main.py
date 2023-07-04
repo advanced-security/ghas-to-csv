9aa41b3c8465bc41578410cbf1878a888c672269
Chris Campbell <808531+ctcampbell@users.noreply.github.com>
2023-07-04 15:50:29 +0100

    Fix lint grumbles

--- main.py
+++ main.py
@@ -21,8 +21,14 @@ from src import code_scanning, dependabot, enterprise, secret_scanning
 import os
 
 # Possible strings indicating feature is not enabled
-secret_scanning_disabled_strings = ["secret scanning is not enabled", "secret scanning is disabled"]
-dependabot_disabled_strings = ["dependabot alerts are not enabled", "dependabot alerts are disabled"]
+secret_scanning_disabled_strings = [
+    "secret scanning is not enabled",
+    "secret scanning is disabled"
+]
+dependabot_disabled_strings = [
+    "dependabot alerts are not enabled",
+    "dependabot alerts are disabled"
+]
 
 # Define the available features
 FEATURES = ["secretscanning", "codescanning", "dependabot"]
